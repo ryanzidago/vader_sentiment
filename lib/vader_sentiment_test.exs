c32a839772466118f4bc71fdf7145bf431a10486
Ryan Zidago <ryanzidago@gmail.com>
2020-07-18 00:19:24 +0200

    added tests for allcaps_differential/1

--- lib/vader_sentiment_test.exs
+++ lib/vader_sentiment_test.exs
@@ -15,4 +15,18 @@ defmodule VaderSentimentTest do
       refute VaderSentiment.negated(["hello", "world"], true)
     end
   end
+
+  describe "allcap_differential/1" do
+    test "returns `true` if a word is upcased while another is downcased" do
+      assert VaderSentiment.allcap_differential(["Biking", "is", "AWESOME"])
+    end
+
+    test "returns `false` if all words are downcased" do
+      refute VaderSentiment.allcap_differential(["eat", "your", "vegetables"])
+    end
+
+    test "returns `false` if all words are upcased" do
+      refute VaderSentiment.allcap_differential(["WORK", "HARD"])
+    end
+  end
 end
